99b587b6ad8753ad2b9ff6f8f8240ecf0b50b3dc
pmanavella <2200292@ucc.edu.ar>
2023-05-17 17:52:51 -0300

    se convirtieron las unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,29 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarUnidades(id, valor) {
+    if (isNaN(valor)) {
+        alert("Se ingreso un valor invalido");
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+    } else if (id == "metro") {
+        document.lasUnidades.unid_pulgada.value = valor * 39.3701;
+        document.lasUnidades.unid_pie.value = valor * 3.28084;
+        document.lasUnidades.unid_yarda.value = valor * 1.09361;
+    } else if (id == "pulgada") {
+        document.lasUnidades.unid_metro.value = valor * 0.0254;
+        document.lasUnidades.unid_pie.value = valor * 0.08333;
+        document.lasUnidades.unid_yarda.value = valor * 0.027778;
+    } else if (id == "pie") {
+        document.lasUnidades.unid_metro.value = valor * 0.3048;
+        document.lasUnidades.unid_pulgada.value = valor * 12;
+        document.lasUnidades.unid_yarda.value = valor * 0.333333;
+    } else if (id == "yarda") {
+        document.lasUnidades.unid_metro.value = valor * 0.9144;
+        document.lasUnidades.unid_pulgada.value = valor * 36;
+        document.lasUnidades.unid_pie.value = valor * 3;
+    }
+}
