0a780b23be35d06a1fbed34822ca794fbcec04f7
pmanavella <2200292@ucc.edu.ar>
2023-05-22 19:59:58 -0300

    se realizo ejercicio de auto

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,12 +9,12 @@
 function cambiarUnidades(id, valor) {
     var metro, pulgada, pie, yarda;
 
-    if(valor.includes(",")) {
-        valor = valor.replace(",",".");
+    if (valor.includes(",")) {
+        valor = valor.replace(",", ".");
     }
 
     if (isNaN(valor)) {
-        alert("Se ingreso un valor invalido " + id );
+        alert("Se ingreso un valor invalido " + id);
         metro = "";
         pulgada = "";
         pie = "";
@@ -41,74 +41,74 @@ function cambiarUnidades(id, valor) {
         pie = valor * 3;
     }
 
-    document.lasUnidades.unid_metro.value = Math.round(metro*100)/100;
-    document.lasUnidades.unid_pulgada.value = Math.round(pulgada*100)/100;
-    document.lasUnidades.unid_pie.value = Math.round(pie*100)/100;
-    document.lasUnidades.unid_yarda.value = Math.round(yarda*100)/100;
+    document.lasUnidades.unid_metro.value = Math.round(metro * 100) / 100;
+    document.lasUnidades.unid_pulgada.value = Math.round(pulgada * 100) / 100;
+    document.lasUnidades.unid_pie.value = Math.round(pie * 100) / 100;
+    document.lasUnidades.unid_yarda.value = Math.round(yarda * 100) / 100;
 }
 
-function convertirGR (id) {
+function convertirGR(id) {
     var grad, rad;
 
     if (id == "grados") {
-        grad = document.getElementById("grados").value ;
-        rad = (grad*Math.PI)/180;
+        grad = document.getElementById("grados").value;
+        rad = (grad * Math.PI) / 180;
     } else if (id == "radianes") {
-        rad = document.getElementById("radianes").value ;
-        grad = (rad*180)/Math.PI;
+        rad = document.getElementById("radianes").value;
+        grad = (rad * 180) / Math.PI;
     }
     document.getElementById("grados").value = grad;
     document.getElementById("radianes").value = rad;
 }
 
-function mostrar_ocultar (valorMO) {
-    if(valorMO== "val_mostrar"){
-        document.getElementById("divMO").style.display = 'block' ;
-    } else if (valorMO=="val_ocultar"){
-        document.getElementById("divMO").style.display = 'none' ;
+function mostrar_ocultar(valorMO) {
+    if (valorMO == "val_mostrar") {
+        document.getElementById("divMO").style.display = 'block';
+    } else if (valorMO == "val_ocultar") {
+        document.getElementById("divMO").style.display = 'none';
     }
 }
 
 function calcularSuma() {
     var num1, num2;
 
-    num1=Number(document.getElementsByName("sum_num1")[0].value);
-    num2=document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].innerHTML=num1+ Number(num2);
+    num1 = Number(document.getElementsByName("sum_num1")[0].value);
+    num2 = document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].innerHTML = num1 + Number(num2);
 }
 
 function calcularResta() {
     var num1, num2;
 
-    num1=Number(document.getElementsByName("res_num1")[0].value);
-    num2=Number(document.getElementsByName("res_num2")[0].value);
+    num1 = Number(document.getElementsByName("res_num1")[0].value);
+    num2 = Number(document.getElementsByName("res_num2")[0].value);
     document.getElementsByName("res_total")[0].innerHTML = num1 - num2;
 }
 
 function calcularProducto() {
     var num1, num2;
 
-    num1=document.getElementsByName("mul_num1")[0].value;
-    num2=document.getElementsByName("mul_num2")[0].value;
-    document.getElementsByName("mul_total")[0].innerHTML= Number(num1)*Number(num2);
+    num1 = document.getElementsByName("mul_num1")[0].value;
+    num2 = document.getElementsByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].innerHTML = Number(num1) * Number(num2);
 }
 
 function calcularCociente() {
     var num1, num2;
 
-    num1=Number(document.getElementsByName("div_num1")[0].value);
-    num2=document.getElementsByName("div_num2")[0].value;
-    document.getElementsByName("div_total")[0].innerHTML=num1/Number(num2);
+    num1 = Number(document.getElementsByName("div_num1")[0].value);
+    num2 = document.getElementsByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].innerHTML = num1 / Number(num2);
 }
 
 function cargarWeb() {
-    var cant, unidad, urlComp ;
+    var cant, unidad, urlComp;
 
-    cant = document.getElementById("distancia").value ;
+    cant = document.getElementById("distancia").value;
     unidad = document.getElementsByName("unidades")[0].value;
 
-    urlComp = "segundaWeb.html#" + cant + "#" + unidad ;
-    window.open(urlComp) ;
+    urlComp = "segundaWeb.html#" + cant + "#" + unidad;
+    window.open(urlComp);
 }
 
 function cargarResultado() {
@@ -124,101 +124,121 @@ function cargarResultado() {
 
 function guardarLocalStorage() {
     let distancia, unidad;
-    distancia = document.getElementById('distancia').value ;
-    unidad = document.getElementsByName('unidades')[0].value ;
-    localStorage.setItem("distanciaLS", distancia) ;
-    localStorage.setItem("unidadesLS", unidad) ;
-    window.open('2_web.html') ;
+    distancia = document.getElementById('distancia').value;
+    unidad = document.getElementsByName('unidades')[0].value;
+    localStorage.setItem("distanciaLS", distancia);
+    localStorage.setItem("unidadesLS", unidad);
+    window.open('2_web.html');
 }
 
 function cargarLocalStorage() {
-    let cant, un ;
-    cant = localStorage.getItem("distanciaLS") ;
-    un = localStorage.getItem("unidadesLS") ;
+    let cant, un;
+    cant = localStorage.getItem("distanciaLS");
+    un = localStorage.getItem("unidadesLS");
 
-    document.getElementById("dist").value = cant + "  " + un ;
+    document.getElementById("dist").value = cant + "  " + un;
 }
 
-function dibujarCirCuad(){
-    var canvas = document.getElementById("myCanvas") ;
-    var ctx = canvas.getContext("2d") ;
-    var xMax = canvas.width ;
-    var yMax = canvas.height ;
-    var margen = 5 ;
+function dibujarCirCuad() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var xMax = canvas.width;
+    var yMax = canvas.height;
+    var margen = 5;
     ctx.fillStyle = "#67dfef"
-    ctx.fillRect(0+margen,yMax-40-margen, 40, 40) ;
+    ctx.fillRect(0 + margen, yMax - 40 - margen, 40, 40);
 
     ctx.fillStyle = "#d267ef"
-    ctx.arc(xMax/2,yMax/2,20,0,2*Math.PI) ;
-    ctx.stroke() ;
-    ctx.fill () ;
+    ctx.arc(xMax / 2, yMax / 2, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
 }
+
 var bandera;
 
 function dibujar(event) {
-    var canvas = document.getElementById("canvasAdibujar") ;
-    var ctx = canvas.getContext("2d") ;
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
 
-    var posX = event.clientX ;
-    var posY = event.clientY ;
+    var posX = event.clientX;
+    var posY = event.clientY;
 
-    console.log(posX,posY) ;
+    console.log(posX, posY);
 
-    canvas.onmousedown = function (){bandera=true} ;
-    canvas.onmouseup = function () {bandera=false} ;
+    canvas.onmousedown = function () {
+        bandera = true
+    };
+    canvas.onmouseup = function () {
+        bandera = false
+    };
 
-    if(bandera) {
-        ctx.fillRect(posX,posY,5,5) ;
-        ctx.fill ;
+    if (bandera) {
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill;
     }
 }
 
 function limpiarCanvas() {
-    var canvas = document.getElementById("canvasAdibujar") ;
-    var ctx = canvas.getContext("2d") ;
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
 
     canvas.width = canvas.width;
 }
 
 function dibujarCuadriculado() {
-    var canvas = document.getElementById("myCanvas") ;
-    var ctx = canvas.getContext("2d") ;
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
 
-    var alturaMax = canvas.height ;
-    var anchoMax = canvas.width ;
+    var alturaMax = canvas.height;
+    var anchoMax = canvas.width;
 
-    ctx.beginPath() ;
-    for (var i=0 ; i<alturaMax ;) {
+    ctx.beginPath();
+    for (var i = 0; i < alturaMax;) {
         ctx.moveTo(0, i);
         ctx.lineTo(anchoMax, i);
         ctx.strokeStyle = "rgb(236,210,245)";
         ctx.stroke();
-        i = i+20 ;
+        i = i + 20;
     }
-    ctx.closePath() ;
+    ctx.closePath();
 
-    ctx.beginPath() ;
-    for (var i=0; i<anchoMax ;) {
+    ctx.beginPath();
+    for (var i = 0; i < anchoMax;) {
         ctx.moveTo(i, 0);
         ctx.lineTo(i, alturaMax);
         ctx.strokeStyle = "(236,210,245)";
         ctx.stroke()
-        i = i+20 ;
+        i = i + 20;
     }
-    ctx.closePath() ;
+    ctx.closePath();
 
-    ctx.beginPath() ;
+    ctx.beginPath();
 
-    ctx.moveTo(0, alturaMax/2);
-    ctx.lineTo(anchoMax, alturaMax/2);
+    ctx.moveTo(0, alturaMax / 2);
+    ctx.lineTo(anchoMax, alturaMax / 2);
     ctx.strokeStyle = "#f1144e";
     ctx.stroke()
 
-    ctx.moveTo(anchoMax/2, 0);
-    ctx.lineTo(anchoMax/2, alturaMax);
+    ctx.moveTo(anchoMax / 2, 0);
+    ctx.lineTo(anchoMax / 2, alturaMax);
     ctx.strokeStyle = "#f1144e";
     ctx.stroke()
 
-    ctx.closePath() ;
+    ctx.closePath();
 
+}
+
+function dibujarImagen(posX, posY) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    console.log(posX, posY);
+    var img = new Image();
+    img.src = "images/auto.png";
+
+    canvas.width = canvas.width;
+
+    img.onload = function () {
+        ctx.drawImage(img, posX, posY);
+    }
 }
