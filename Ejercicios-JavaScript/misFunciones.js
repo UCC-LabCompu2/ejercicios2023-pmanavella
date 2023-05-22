192a6552ea721f346fee27093a0e02a71af311cc
pmanavella <2200292@ucc.edu.ar>
2023-05-22 15:32:17 -0300

    ejercicio inicial de canvas, dibujar

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -138,3 +138,18 @@ function cargarLocalStorage() {
 
     document.getElementById("dist").value = cant + "  " + un ;
 }
+
+function dibujarCirCuad(){
+    var canvas = document.getElementById("myCanvas") ;
+    var ctx = canvas.getContext("2d") ;
+    var xMax = canvas.width ;
+    var yMax = canvas.height ;
+    var margen = 5 ;
+    ctx.fillStyle = "#67dfef"
+    ctx.fillRect(0+margen,yMax-40-margen, 40, 40) ;
+
+    ctx.fillStyle = "#d267ef"
+    ctx.arc(xMax/2,yMax/2,20,0,2*Math.PI) ;
+    ctx.stroke() ;
+    ctx.fill () ;
+}
