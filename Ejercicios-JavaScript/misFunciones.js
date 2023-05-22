ea690f5e2717678d59e91290b35dab1b4b11be67
pmanavella <2200292@ucc.edu.ar>
2023-05-22 15:44:00 -0300

    creacion de Paint

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -153,3 +153,29 @@ function dibujarCirCuad(){
     ctx.stroke() ;
     ctx.fill () ;
 }
+var bandera;
+
+function dibujar(event) {
+    var canvas = document.getElementById("canvasAdibujar") ;
+    var ctx = canvas.getContext("2d") ;
+
+    var posX = event.clientX ;
+    var posY = event.clientY ;
+
+    console.log(posX,posY) ;
+
+    canvas.onmousedown = function (){bandera=true} ;
+    canvas.onmouseup = function () {bandera=false} ;
+
+    if(bandera) {
+        ctx.fillRect(posX,posY,5,5) ;
+        ctx.fill ;
+    }
+}
+
+function limpiarCanvas() {
+    var canvas = document.getElementById("canvasAdibujar") ;
+    var ctx = canvas.getContext("2d") ;
+
+    canvas.width = canvas.width;
+}
